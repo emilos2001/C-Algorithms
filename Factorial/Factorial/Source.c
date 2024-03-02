4bd782ed5c482167d0f92b2112f44458be42f246
Emil Bogaci <140043844+emilos2001@users.noreply.github.com>
2024-03-02 14:52:15 +0200

    Update Source.c

--- Factorial/Factorial/Source.c
+++ Factorial/Factorial/Source.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main() {
 	int n;
-	printf("enter a number: : ");
+	printf("Enter a number : ");
 	scanf_s("%d", &n);
 	int s = factorial(n);
 	printf("The factorial of the number %d is: %d\n", n, s);
@@ -9,6 +9,13 @@ int main() {
 }
 
 int factorial(int n) {
+	if (n < 0) {
+	printf("Please enter a non-negative integer.\n");
+	printf("Enter a number : ");
+	scanf_s("%d", &n);
+	return factorial(n);
+	}
+	
 	if (n == 0 || n == 1) 
 	{
 		return 1;
