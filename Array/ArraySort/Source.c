80071bc20f16ea72ea8466c6d6689d3e3f0f446a
Emil Bogaci <140043844+emilos2001@users.noreply.github.com>
2024-03-02 14:17:40 +0200

    Update Source.c

--- Array/ArraySort/Source.c
+++ Array/ArraySort/Source.c
@@ -1,16 +1,14 @@
 #include<stdio.h>
 
 int main() {
-	
-	int arr[] = {5, 6, 1 ,9, 2, 20, 11, 18};
-	int n = sizeof(arr) / sizeof(arr[0]);
+    int arr[] = {5, 6, 1 ,9, 2, 20, 11, 18};
+    int n = sizeof(arr) / sizeof(arr[0]);
     printf("initial array : ");
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
-
-	int max = 0;
-	int min = 0;
+    int max = 0;
+    int min = 0;
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] < arr[j + 1]) {
@@ -33,7 +31,7 @@ int main() {
         }
     }
 
-	min = arr[0];
+    min = arr[0];
     printf("\ninitial array in ascendent order : ");
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
@@ -43,7 +41,7 @@ int main() {
         printf("%d ", arr[i]);
     }
 
-	printf("\nMax number of this array is : %d", max);
-	printf("\nMin number of this array is : %d", min);
+    printf("\nMax number of this array is : %d", max);
+    printf("\nMin number of this array is : %d", min);
 	return 0;
 }
